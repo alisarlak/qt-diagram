fa9e7ad991472480239a799332179f4c11f2a5a0
super <ali.sarlak@aol.com>
2016-08-17 09:53:52 +0430

    NO MSG

---
+++ src/test.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "test.h"
+
+int main()
+{
+	//print in STD output
+	printf("Press any key to continue");
+	return 0;
+}
---
+++ src/test.h
@@ -0,0 +1,17 @@
+#ifndef TEST_H_
+#define TEST_H_
+//I just want to check GIT repository
+
+
+
+class CWSD
+{
+private :
+	int m_attribute;
+public:
+	CWSD():{m_attribute(0)};
+	~CWSD();
+}
+
+
+#endif
